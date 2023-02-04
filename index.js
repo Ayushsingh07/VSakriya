6122733814a112c1a11556a955e30b4e101980b1
Ayushsingh07 <nqu7069@gmail.com>
2023-02-04 11:54:42 +0530

    dd

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ const policeroute = require("./routes/Police");
 const carroute = require("./routes/Car");
 const cors=require('cors')
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cors()) 
+
+
+
+
 // configurations
 app.use(express.json());
 dotenv.config();
+
+
+
 
 // constants
 const PORT = process.env.PORT || 3000;
